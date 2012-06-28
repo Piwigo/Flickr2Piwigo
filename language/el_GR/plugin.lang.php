c7d9ab73aa3623f711d0a17b9049c3bbafd91720
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-28 04:19:18 +0000

    [flickr] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@16107 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d albums'] = '%d λευκώματα';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d των εικόνων δεν εμφανίζονται επειδή υπάρχουν ήδη στην βάση δεδομένων.';
+$lang['%d pictures imported'] = '%d εικόνες εισήχθησαν';
+$lang['%s imported'] = '%s εισήχθησαν';
+$lang['(%d photos)'] = '(%d εικόνες)';
+$lang['API key'] = 'κλειδί API ';
+$lang['API not authenticated'] = 'το κλειδί API δεν επικυρώθηκε';
+$lang['API secret'] = 'API μυστικό';
+$lang['Begin transfer'] = 'Ξεκινήστε τη μεταφορά';
+$lang['Callback URL'] = 'URL ανάκλησης';
+$lang['Fill these fields from Flickr datas'] = 'Συμπληρώστε αυτά τα πεδία από τη βάση δεδομένων του Flickr';
+$lang['Flickr logins'] = 'Flickr συνδέσεις';
+$lang['How do I get my Flickr API key?'] = 'Πώς μπορώ να αποκτήσω το Flickr API κλειδί μου;';
+$lang['Import all my pictures'] = 'Εισαγωγή όλες τις φωτογραφίες μου';
+$lang['Import all photos in this album'] = 'Εισαγωγή όλες τις φωτογραφίες από αυτό το λεύκωμα';
+$lang['Import options'] = 'επιλογές εισαγωγής';
+$lang['Import'] = 'Εισαγωγή';
+$lang['List my albums'] = 'Εμφάνισε τα άλμπουμ μου';
+$lang['List pictures of this album'] = 'Εμφάνισε φωτογραφίες αυτού του λευκώματος';
+$lang['Logued as'] = 'Καταγράφηκε ως';
+$lang['No download method available'] = 'Δεν υπάρχει μέθοδος λήψης διαθέσιμη';
+$lang['One picture is not displayed because already existing in the database.'] = 'Μια εικόνα δεν εμφανίζεται επειδή υπάρχει ήδη στην βάση δεδομένων.';
+$lang['Open Flickr page in a new tab'] = 'ανοίξτε τη σελίδα του Flickr σε νέα καρτέλα';
+$lang['Pictures without album'] = 'Εικόνες χωρίς λεύκωμα';
+$lang['Please fill your API keys on the configuration tab'] = 'Παρακαλούμε συμπληρώστε τα κλειδιά API σας στην καρτέλα Διαμόρφωση';
+$lang['Processing...'] = 'Επεξεργασία ...';
+$lang['Reproduce flickr albums'] = 'Αναπαραγωγή λευκώματος flickr ';
+$lang['Successfully logued to you Flickr account'] = 'Συνδεθήκατε επιτυχώς στο Flickr λογαριασμό σας';
+?>
