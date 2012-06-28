b56145c5e9bc35eb485bde226eec413deba6db50
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-28 16:03:16 +0000

    [flickr] Update ru_RU, thanks to : nadusha
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@16128 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d albums'] = 'Альбомов: %d';
+$lang['%d pictures are not displayed because already existing in the database.'] = 'Не показано фотографий: %d (они уже есть в базе данных).';
+$lang['%d pictures imported'] = 'Фотографий импортировано: %d';
+$lang['%s imported'] = '%s импортирован';
+$lang['(%d photos)'] = '(фотографий: %d)';
+$lang['API key'] = 'Ключ API';
+$lang['API not authenticated'] = 'API не авторизован';
+$lang['API secret'] = 'Шифр API';
+$lang['Begin transfer'] = 'Начать передачу';
+$lang['Callback URL'] = 'Обратный адрес URL';
+$lang['Fill these fields from Flickr datas'] = 'Заполните эти поля из Flickr Datas';
+$lang['Flickr logins'] = 'Вход во Flikr';
+$lang['How do I get my Flickr API key?'] = 'Как я могу получить ключ Flickr API?';
+$lang['Import all my pictures'] = 'Импортировать все фотографии';
+$lang['Import all photos in this album'] = 'Импортировать все фотографии из этого альбома';
+$lang['Import options'] = 'Параметры импорта';
+$lang['Import'] = 'Импорт';
+$lang['List my albums'] = 'Список моих альбомов';
+$lang['List pictures of this album'] = 'Список фотографий альбома';
+$lang['Logued as'] = 'Авторизован как';
+$lang['No download method available'] = 'Нет доступного метода загрузки';
+$lang['One picture is not displayed because already existing in the database.'] = 'Некоторые картинки не отображается, так как уже существуют в базе данных.';
+$lang['Open Flickr page in a new tab'] = 'Открыть страницу Flickr в новой вкладке';
+$lang['Pictures without album'] = 'Фотографии без альбома';
+$lang['Please fill your API keys on the configuration tab'] = 'Пожалуйста, заполните ключ API на вкладке конфигурации';
+$lang['Processing...'] = 'Обработка ...';
+$lang['Reproduce flickr albums'] = 'Воспроизведение альбомов Flickr';
+$lang['Successfully logued to you Flickr account'] = 'Вы успешно вошли в свою учетную запись';
+?>
