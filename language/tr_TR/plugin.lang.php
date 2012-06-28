953d29f208dacdc82cabdeeab6db7015215f2c38
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-28 16:03:32 +0000

    [flickr] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@16130 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d albums'] = '%d albüm';
+$lang['%d pictures are not displayed because already existing in the database.'] = 'Mevcut veri tabanında zaten bulunduklarından %d resim gösterilmedi.';
+$lang['%d pictures imported'] = '%d resim içe aktarıldı';
+$lang['%s imported'] = '%s içe aktarıldı';
+$lang['(%d photos)'] = '(%d fotoğraf)';
+$lang['API key'] = 'API anahtarı';
+$lang['API not authenticated'] = 'API doğrulanmamış';
+$lang['API secret'] = 'API gizli';
+$lang['Begin transfer'] = 'Aktarmaya başla';
+$lang['Callback URL'] = 'URL \'yi geri çağır';
+$lang['Fill these fields from Flickr datas'] = 'Bu alanları Flickr bilgilerine göre doldurun';
+$lang['Flickr logins'] = 'Flickr oturumları';
+$lang['How do I get my Flickr API key?'] = 'Flickr API anahtarını nasıl alabilirim ?';
+$lang['Import all my pictures'] = 'Tüm resimlerimi içe aktar';
+$lang['Import all photos in this album'] = 'Bu albümdeki tüm fotoğrafları içe aktar';
+$lang['Import options'] = 'İçe aktarma seçenekleri';
+$lang['Import'] = 'İçe aktar';
+$lang['List my albums'] = 'Albümlerimi listele';
+$lang['List pictures of this album'] = 'Bu albümdeki resimleri listele';
+$lang['Logued as'] = 'Oturum açan:';
+$lang['No download method available'] = 'İndirme yöntemi mevcut değil';
+$lang['One picture is not displayed because already existing in the database.'] = 'Mevcut veri tabanında zaten bulunduğundan bir resim gösterilmedi.';
+$lang['Open Flickr page in a new tab'] = 'Flickr sayfasını yeni sekmede aç';
+$lang['Pictures without album'] = 'Albümde olmayan resimler';
+$lang['Please fill your API keys on the configuration tab'] = 'Lütfen yapılandırma sayfasındaki API anahtarlarını giriniz';
+$lang['Processing...'] = 'İşleniyor...';
+$lang['Reproduce flickr albums'] = 'Flickr albümlerini yeniden üret';
+$lang['Successfully logued to you Flickr account'] = 'Flickr \'de başarılı olarak oturum açıldı.';
+?>
