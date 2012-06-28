e13cfe95ee91c310c7a1b21e1ab40f6b7370068d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-28 04:19:39 +0000

    [flickr] Update nl_NL, thanks to : ruud
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@16108 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d albums'] = '%d albums';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d afbeeldingen worden niet getoond omdat ze al in de database staan.';
+$lang['%d pictures imported'] = '%d afbeeldingen geïmporteerd';
+$lang['%s imported'] = '%s geïmporteerd';
+$lang['(%d photos)'] = '(%d foto\'s)';
+$lang['API key'] = 'API sleutel';
+$lang['API not authenticated'] = 'API niet geverifieerd';
+$lang['API secret'] = 'API geheim';
+$lang['Begin transfer'] = 'Begin overdracht';
+$lang['Callback URL'] = 'Callback URL';
+$lang['Fill these fields from Flickr datas'] = 'Vul deze velden met Flickr data';
+$lang['Flickr logins'] = 'Flickrs logins';
+$lang['How do I get my Flickr API key?'] = 'Hoe verkrijg ik mijn Flickr API sleutel?';
+$lang['Import all my pictures'] = 'Importeer al mijn afbeeldingen';
+$lang['Import all photos in this album'] = 'Importeer alle foto\'s in dit album';
+$lang['Import options'] = 'Import opties';
+$lang['Import'] = 'Import';
+$lang['List my albums'] = 'Maak een lijst met mijn albums';
+$lang['List pictures of this album'] = 'Maak een lijst met afbeeldingen van dit album';
+$lang['Logued as'] = 'Ingelogd als';
+$lang['No download method available'] = 'Geen download aanpak voorhanden';
+$lang['One picture is not displayed because already existing in the database.'] = 'Een afbeelding wordt niet getoond omdat het al in de database aanwezig is.';
+$lang['Open Flickr page in a new tab'] = 'Open de Flickr pagina in een nieuwe tab';
+$lang['Pictures without album'] = 'Afbeeldingen zonder album';
+$lang['Please fill your API keys on the configuration tab'] = 'Vul je API sleutels in in de configuratietab';
+$lang['Processing...'] = 'Bewerken ...';
+$lang['Reproduce flickr albums'] = 'Reproduceer Flickr albums';
+$lang['Successfully logued to you Flickr account'] = 'Succesvol ingelogd in Flickr ';
+?>
