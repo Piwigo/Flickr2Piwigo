868f1782cebda20add2f5e7d6266b105ab6ced73
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-28 16:03:09 +0000

    [flickr] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@16127 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d albums'] = '%d albūmi';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d netiek rādītas, jo datubāzē jau eksistē.';
+$lang['%d pictures imported'] = '%d attēli importēti';
+$lang['%s imported'] = '%s importēti';
+$lang['(%d photos)'] = '(%d attēlu)';
+$lang['API key'] = 'API atsēga';
+$lang['API not authenticated'] = 'API nav autentisks';
+$lang['API secret'] = 'API slepens';
+$lang['Begin transfer'] = 'Sākt pārsūtīšanu';
+$lang['Callback URL'] = 'Atbildes URL';
+$lang['Fill these fields from Flickr datas'] = 'Aizpildīt šos laukus ar Flickr datiem';
+$lang['Flickr logins'] = 'Flickr pieteikšanās';
+$lang['How do I get my Flickr API key?'] = 'Kā es varu iegūt savu Flickr API atslēgu?';
+$lang['Import all my pictures'] = 'Importēt visas manas bildes';
+$lang['Import all photos in this album'] = 'Importēt visus attēlus šajā albūmā';
+$lang['Import options'] = 'Importēšanas iespējas';
+$lang['Import'] = 'Importēt';
+$lang['List my albums'] = 'Sastādīt manu albūmu sarakstu';
+$lang['List pictures of this album'] = 'Sastādīt šā albūma attēlu sarakstu';
+$lang['Logued as'] = 'Pieteicies kā';
+$lang['No download method available'] = 'Lejupielādes metodes nav pieejamas';
+$lang['One picture is not displayed because already existing in the database.'] = 'Attēls netika parādīts, jo datubāzē jau tāds eksistē';
+$lang['Open Flickr page in a new tab'] = 'Atvērt Flickr lapu jaunā cilnē';
+$lang['Pictures without album'] = 'Attēli, kas nav albūmos ';
+$lang['Please fill your API keys on the configuration tab'] = 'Lūdzu ieraksties savas API atslēgas konfigurācijas cilnē';
+$lang['Processing...'] = 'Apstrāde...';
+$lang['Reproduce flickr albums'] = 'Reproducēt Flickr albūmus';
+$lang['Successfully logued to you Flickr account'] = 'Esat veiksmīgi pieteikušies savā Flickr kontā';
+?>
