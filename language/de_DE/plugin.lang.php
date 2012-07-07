52746d7d5f77a2603909cb7b68d81be4f066d180
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-07 14:07:55 +0000

    [flickr] Update de_DE, thanks to : Joergen
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@16450 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Import all my pictures'] = 'Importiert alle meine Bilder';
+$lang['Import all photos in this album'] = 'Importiert alle Fotos in das Album';
+$lang['Import options'] = 'Import Optionen';
+$lang['Import'] = 'Importieren';
+$lang['List my albums'] = 'Album aufführen';
+$lang['List pictures of this album'] = 'Bilder des Album aufführen';
+?>
