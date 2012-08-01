ccefbe6a48112be7391d75bfd4b4abcc72fb4a0f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-01 05:58:06 +0000

    [flickr] Update uk_UA, thanks to :
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@17256 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Callback URL'] = 'Зворотній виклик URL (Callback URL)';
+$lang['One picture is not displayed because already existing in the database.'] = 'Одне зображення не відображається, тому що вже існує в базі даних.';
+$lang['Please fill your API keys on the configuration tab'] = 'Будь ласка, заповніть API ключі на вкладці конфігурації';
+$lang['Processing...'] = 'Обробка...';
+$lang['Reproduce flickr albums'] = 'Відтворити flickr альбоми';
+$lang['Successfully logued to you Flickr account'] = 'Успішно увійшли до вашого облікового запису Flickr';
+$lang['(%d photos)'] = '(%d зображень)';
+$lang['API key'] = 'Ключ API';
+$lang['API not authenticated'] = 'API не перевірку автентичності';
+$lang['API secret'] = 'Секретний API';
+$lang['Begin transfer'] = 'Почати передачу';
+$lang['Fill these fields from Flickr datas'] = 'Заповніть ці поля з даних Flickr';
+$lang['Flickr logins'] = 'Логіни Flickr';
+$lang['How do I get my Flickr API key?'] = 'Як отримати ключ Flickr API?';
+$lang['Import'] = 'Імпортувати';
+$lang['Import all my pictures'] = 'Імпортувати всі мої зображення';
+$lang['Import all photos in this album'] = 'Імпортувати всі зображення в цей альбом';
+$lang['Import options'] = 'Параметри імпорту';
+$lang['List my albums'] = 'Список моїх альбомів';
+$lang['List pictures of this album'] = 'Список зображень цього альбому';
+$lang['Logued as'] = 'Залоґінений як';
+$lang['No download method available'] = 'Немає доступних методів завантаження';
+$lang['Open Flickr page in a new tab'] = 'Відкрити сторінку Flickr в новій вкладці';
+$lang['Pictures without album'] = 'Зображення без альбому';
+$lang['%d albums'] = '%d альбоми';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d малюнки не відображаються, оскільки вже існують в базі даних.';
+$lang['%d pictures imported'] = '%d імпортованих зображень';
+$lang['%s imported'] = '%s імпортовано';
+?>
