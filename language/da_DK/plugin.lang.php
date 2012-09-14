d73ae8a1df081c148d1119e7c4718d12aaa882bf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-14 19:20:16 +0000

    [flickr] Update da_DK, thanks to : Egorip
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@17917 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Logued out'] = 'logget ud';
+$lang['No download method available'] = 'Ingen download metode til rådighed';
+$lang['One picture is not displayed because already existing in the database.'] = 'Et billede vises ikke, fordi der allerede findes i databasen.';
+$lang['Open Flickr page in a new tab'] = 'Åbn Flickr side i en ny fane';
+$lang['Photo "%s" imported'] = 'Foto "%s" importerede';
+$lang['Pictures without album'] = 'Billeder uden album';
+$lang['Please fill your API keys on the configuration tab'] = 'Udfyld dine API nøgler på fanen Konfiguration';
+$lang['Processing...'] = 'Behandler ...';
+$lang['Reproduce flickr albums'] = 'Reproducere flickr album';
+$lang['Successfully logued to you Flickr account'] = 'Succesfuld logget på din Flickr-konto';
+$lang['%d albums'] = '%d album';
+$lang['%d elements ready for importation'] = '%d elementer klar til import';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d billeder vises ikke, fordi der allerede findes i databasen.';
+$lang['%d pictures imported'] = '%d billeder er importeret';
+$lang['(%d photos)'] = '(%d fotos)';
+$lang['API key'] = 'API nøgle';
+$lang['API not authenticated'] = 'API ikke godkendt';
+$lang['API secret'] = 'API hemmelig';
+$lang['Begin transfer'] = 'begynd overførsel';
+$lang['Callback URL'] = 'tilbagekald URL';
+$lang['Fill these fields from Flickr datas'] = 'Fyld disse felter fra Flickr datas';
+$lang['Flickr logins'] = 'Flickr logins';
+$lang['How do I get my Flickr API key?'] = 'Hvordan får jeg min Flickr API-nøgle?';
+$lang['Import'] = 'Import';
+$lang['Import all my pictures'] = 'Import alle mine billeder';
+$lang['Import all photos in this album'] = 'Import all photos in this album';
+$lang['Import options'] = 'importindstillinger';
+$lang['List my albums'] = 'Liste mine albums';
+$lang['List pictures of this album'] = 'List billeder af dette album';
+$lang['Logued as'] = 'logget som';
+?>
