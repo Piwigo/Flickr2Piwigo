3040354a2f016ac35bff26f3122b612240aafd83
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-18 18:55:42 +0000

    [flickr] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@17992 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -20,34 +20,34 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Logued out'] = 'logget ud';
-$lang['No download method available'] = 'Ingen download metode til rådighed';
-$lang['One picture is not displayed because already existing in the database.'] = 'Et billede vises ikke, fordi der allerede findes i databasen.';
-$lang['Open Flickr page in a new tab'] = 'Åbn Flickr side i en ny fane';
-$lang['Photo "%s" imported'] = 'Foto "%s" importerede';
+$lang['Logued out'] = 'Logget ud';
+$lang['No download method available'] = 'Ingen downloadmetode til rådighed';
+$lang['One picture is not displayed because already existing in the database.'] = 'Et billede vises ikke, fordi det allerede findes i databasen.';
+$lang['Open Flickr page in a new tab'] = 'Åbn Flickr-side i en ny fane';
+$lang['Photo "%s" imported'] = 'Importerede fotoet "%s"';
 $lang['Pictures without album'] = 'Billeder uden album';
-$lang['Please fill your API keys on the configuration tab'] = 'Udfyld dine API nøgler på fanen Konfiguration';
+$lang['Please fill your API keys on the configuration tab'] = 'Angiv dine API-nøgler på opsætningsfanen';
 $lang['Processing...'] = 'Behandler ...';
-$lang['Reproduce flickr albums'] = 'Reproducere flickr album';
-$lang['Successfully logued to you Flickr account'] = 'Succesfuld logget på din Flickr-konto';
+$lang['Reproduce flickr albums'] = 'Reproducere Flickr-album';
+$lang['Successfully logued to you Flickr account'] = 'Loggede på din Flickr-konto';
 $lang['%d albums'] = '%d album';
 $lang['%d elements ready for importation'] = '%d elementer klar til import';
-$lang['%d pictures are not displayed because already existing in the database.'] = '%d billeder vises ikke, fordi der allerede findes i databasen.';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d billeder vises ikke, fordi de allerede findes i databasen.';
 $lang['%d pictures imported'] = '%d billeder er importeret';
-$lang['(%d photos)'] = '(%d fotos)';
-$lang['API key'] = 'API nøgle';
+$lang['(%d photos)'] = '(%d fotoer)';
+$lang['API key'] = 'API-nøgle (key)';
 $lang['API not authenticated'] = 'API ikke godkendt';
-$lang['API secret'] = 'API hemmelig';
+$lang['API secret'] = 'API-hemmelighed (secret)';
 $lang['Begin transfer'] = 'begynd overførsel';
-$lang['Callback URL'] = 'tilbagekald URL';
-$lang['Fill these fields from Flickr datas'] = 'Fyld disse felter fra Flickr datas';
-$lang['Flickr logins'] = 'Flickr logins';
-$lang['How do I get my Flickr API key?'] = 'Hvordan får jeg min Flickr API-nøgle?';
-$lang['Import'] = 'Import';
-$lang['Import all my pictures'] = 'Import alle mine billeder';
-$lang['Import all photos in this album'] = 'Import all photos in this album';
-$lang['Import options'] = 'importindstillinger';
-$lang['List my albums'] = 'Liste mine albums';
-$lang['List pictures of this album'] = 'List billeder af dette album';
-$lang['Logued as'] = 'logget som';
+$lang['Callback URL'] = 'tilbagekalds-URL';
+$lang['Fill these fields from Flickr datas'] = 'Udfyld disse felter med Flickr-data';
+$lang['Flickr logins'] = 'Flickr-logins';
+$lang['How do I get my Flickr API key?'] = 'Hvordan finder jeg min Flickr API-nøgle?';
+$lang['Import'] = 'Importer';
+$lang['Import all my pictures'] = 'Importer alle mine billeder';
+$lang['Import all photos in this album'] = 'Importer alle fotoer i dette album';
+$lang['Import options'] = 'Importindstillinger';
+$lang['List my albums'] = 'Liste over mine albums';
+$lang['List pictures of this album'] = 'List billeder i dette album';
+$lang['Logued as'] = 'Logget som';
 ?>
