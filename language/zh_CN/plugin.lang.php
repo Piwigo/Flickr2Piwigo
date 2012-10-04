68dac18a8da735698a965e5d9203d1a35af2f963
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-04 17:29:58 +0000

    [flickr] Update zh_CN, thanks to : quqibing
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@18471 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Logued as'] = '登录为';
+$lang['Logued out'] = '登出';
+$lang['List my albums'] = '列出我的相册';
+$lang['List pictures of this album'] = '列出相册图片';
+$lang['No download method available'] = '没有可用的下载方法';
+$lang['One picture is not displayed because already existing in the database.'] = '一张图片没有显示，因为已经存在于数据库中。';
+$lang['Open Flickr page in a new tab'] = '在新的标签页打开Flickr';
+$lang['Photo "%s" imported'] = '导入了图片"%s"';
+$lang['Pictures without album'] = '没有相册的图片';
+$lang['Please fill your API keys on the configuration tab'] = '请填写您的API密钥到配置选项卡上';
+$lang['Processing...'] = '处理中...';
+$lang['Reproduce flickr albums'] = '复制Flickr相册';
+$lang['Successfully logued to you Flickr account'] = '成功登录到Flickr相册';
+$lang['(%d photos)'] = '(%d张图片)';
+$lang['API key'] = 'API key';
+$lang['API not authenticated'] = 'API未通过验证';
+$lang['API secret'] = 'API密钥';
+$lang['Begin transfer'] = '开始转换';
+$lang['Callback URL'] = '回调URL';
+$lang['Fill these fields from Flickr datas'] = '用来自Flickr的数据填写这些字段';
+$lang['Flickr logins'] = 'Flickr登录';
+$lang['How do I get my Flickr API key?'] = '我如何获得Flickr API key';
+$lang['Import'] = '导入';
+$lang['Import all my pictures'] = '导入我所有的照片';
+$lang['Import all photos in this album'] = '导入这个相册中所有图片';
+$lang['Import options'] = '导入选项';
+$lang['%d albums'] = '%d个相册';
+$lang['%d elements ready for importation'] = '将要导入%d元素';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d张图片没有显示，因为已经存在于数据库中。';
+$lang['%d pictures imported'] = '已经导入%d张图片';
+?>
