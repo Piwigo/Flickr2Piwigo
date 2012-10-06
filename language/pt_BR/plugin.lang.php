7cc95381210db0b60ade34c47d1671b65ee6836f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-06 16:03:48 +0000

    [flickr] Update pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@18529 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Logued out'] = 'Deslogar';
+$lang['No download method available'] = 'Nenhum método de download disponível';
+$lang['One picture is not displayed because already existing in the database.'] = 'Uma foto não foi mostrada porque já existe no banco de dados';
+$lang['Open Flickr page in a new tab'] = 'Abre página do Flickr em uma nova aba';
+$lang['Photo "%s" imported'] = 'Foto "%s" importada';
+$lang['Pictures without album'] = 'Foto sem álbum';
+$lang['Please fill your API keys on the configuration tab'] = 'Por favor preencha sua chave do API do Flickr na aba de configuração';
+$lang['Processing...'] = 'Processando...';
+$lang['Reproduce flickr albums'] = 'Reproduza álbuns do Flickr';
+$lang['Successfully logued to you Flickr account'] = 'Logado com sucesso na sua conta do Flickr';
+$lang['%d albums'] = '%d albuns';
+$lang['%d elements ready for importation'] = '%d elementos prontos para importação';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d fotos não foram mostradas porque já existem no banco de dados';
+$lang['%d pictures imported'] = '%d fotos importadas';
+$lang['(%d photos)'] = '(%d fotos)';
+$lang['API key'] = 'Chave API';
+$lang['API not authenticated'] = 'API não autenticada';
+$lang['API secret'] = 'API secreta';
+$lang['Begin transfer'] = 'Inicia transferência';
+$lang['Callback URL'] = 'URL Retorno';
+$lang['Fill these fields from Flickr datas'] = 'Preencha esses campos com dados do Flickr';
+$lang['Flickr logins'] = 'Loguin do Flick';
+$lang['How do I get my Flickr API key?'] = 'Como eu obtenho minha chave da API do Flickr';
+$lang['Import'] = 'Importa';
+$lang['Import all my pictures'] = 'Importa todas as minhas fotos';
+$lang['Import all photos in this album'] = 'Importa todas as fotos nesse álbum';
+$lang['Import options'] = 'opções de importação';
+$lang['List my albums'] = 'Lista meus álbuns';
+$lang['List pictures of this album'] = 'Lista fotos nesse álbum';
+$lang['Logued as'] = 'Logado como';
+?>
