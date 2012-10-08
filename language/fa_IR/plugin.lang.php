d750599b03ade17e4e7e40ae9f66101822b5988d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-08 16:06:35 +0000

    [flickr] Update fa_IR, thanks to : arashshiler
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@18553 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d pictures imported'] = '%d  عکس های وارد شده';
+$lang['Processing...'] = 'در حال پردازش ...';
+$lang['Pictures without album'] = 'عکس ها بدون آلبوم';
+$lang['%d albums'] = '%d آلبوم ها';
+$lang['(%d photos)'] = '(%d عکس ها)';
+$lang['Flickr logins'] = 'ورود به فلیکر';
+$lang['Import all my pictures'] = 'وارد کردن همه ی عکس های من';
+$lang['Import all photos in this album'] = 'وارد کردن همه عکس ها در این آلبوم';
+$lang['Import options'] = 'وارد کردن گزینه';
+$lang['List my albums'] = 'لیست البوم های من';
+$lang['List pictures of this album'] = 'لیست عکس ها از این البوم';
+$lang['Import'] = 'وارد کردن';
+$lang['Begin transfer'] = 'آغاز انتقال';
+?>
