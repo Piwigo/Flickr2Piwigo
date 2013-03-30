59cc95b9ec11e472684f62ffb998f4699a3ccee8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-30 22:46:26 +0000

    [flickr] Add bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@21918 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Import all photos in this album'] = 'Импортиране на всички снимки от този албум';
+$lang['Import all my pictures'] = 'Импортиране на всички мои снимки';
+$lang['How do I get my Flickr API key?'] = 'Как да взема моя API ключ?';
+$lang['Fill these fields from Flickr datas'] = 'Попълнете тези полета за Flickr данни';
+$lang['Callback URL'] = 'Адрес за отговор';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d снимки не са показани защото вече съществуват в базата данни';
+$lang['%d elements ready for importation'] = '%d елементи готови за импортиране';
+$lang['Please fill your API keys on the configuration tab'] = 'Моля въведете вашия API ключ в конфигурационното поле';
+$lang['Pictures without album'] = 'Снимки без албум';
+$lang['Photo "%s" imported'] = 'Снимка "%s" качена';
+$lang['Open Flickr page in a new tab'] = 'Отвори Flickr в нов прозорец';
+$lang['One picture is not displayed because already existing in the database.'] = 'Една снимка не се показва защото вече е налична в база данни';
+$lang['No download method available'] = 'Няма наличен метод за сваляне';
+$lang['Begin transfer'] = 'Начало на трансфер';
+$lang['API secret'] = 'таен API';
+$lang['(%d photos)'] = '(%d снимки)';
+$lang['Successfully logged in to you Flickr account'] = 'Успешно логнат във вашия Flickr акаунт';
+$lang['Reproduce flickr albums'] = 'Презаписват се flickr албумите';
+$lang['Processing...'] = 'Изпълнява се...';
+$lang['Logged out'] = 'Излизане';
+$lang['Logged in as'] = 'Логнат като';
+$lang['List pictures of this album'] = 'Последна снимка в този албум';
+$lang['List my albums'] = 'Списък с моите алубми';
+$lang['Import options'] = 'Опции за импортиране';
+$lang['Import'] = 'Импортиране';
+$lang['Flickr logins'] = 'Flickr логване';
+$lang['%d albums'] = '%d албуми';
+$lang['%d pictures imported'] = '%d снимки импортирани';
+$lang['API not authenticated'] = 'API ключа не е удостоверен';
+$lang['API key'] = 'API ключ';
+?>
