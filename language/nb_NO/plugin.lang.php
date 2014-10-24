b3532b1aac50aeb27928d1d4d221e00b646fadef
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-24 17:08:03 +0000

    [flickr] Add nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/flickr2piwigo@30288 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,53 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Logged in as'] = 'Logget inn som';
+$lang['Logged out'] = 'Logget ut';
+$lang['No download method available'] = 'Ingen nedlastning metode tilgjengelig';
+$lang['One picture is not displayed because already existing in the database.'] = 'Ett bilde vises ikke fordi det allerede er eksisterende i databasen.';
+$lang['Open Flickr page in a new tab'] = 'Åpne Flickr siden i en ny fane';
+$lang['Photo "%s" imported'] = 'Bilde "%s" importert';
+$lang['Pictures without album'] = 'Bilder uten album';
+$lang['Please fill your API keys on the configuration tab'] = 'Vennligst fyll inn API nøkkelen i konfigurasjon fanen';
+$lang['Processing...'] = 'Behandler ...';
+$lang['Reproduce flickr albums'] = 'Reprodusere Flickr-album';
+$lang['Successfully logged in to you Flickr account'] = 'Vellykket pålogget din Flickr-konto';
+$lang['%d albums'] = '%d albumer';
+$lang['%d elements ready for importation'] = '%d elementer klar for innførsel';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d bilder vises ikke fordi de er allerede eksisterende i databasen.';
+$lang['%d pictures imported'] = '%d bilder importert';
+$lang['(%d photos)'] = '(%d bilder)';
+$lang['API key'] = 'API nøkkel';
+$lang['API not authenticated'] = 'API ikke bekreftet';
+$lang['API secret'] = 'API hemmelig';
+$lang['Begin transfer'] = 'Begynn overføring';
+$lang['Callback URL'] = 'Tilbakekall URL';
+$lang['Fill these fields from Flickr datas'] = 'Fyll ut disse feltene fra Flickr data';
+$lang['Flickr logins'] = 'Flickr innlogginger';
+$lang['Geolocalization'] = 'Geolokalisering';
+$lang['How do I get my Flickr API key?'] = 'Hvordan får jeg tak på min Flickr Api Nøkkel?';
+$lang['Import'] = 'Importere';
+$lang['Import all my pictures'] = 'Importere alle mine bilder';
+$lang['Import all photos in this album'] = 'Importere alle bilder i denne album';
+$lang['Import options'] = 'Import valg';
+$lang['List my albums'] = 'Liste mine album';
+$lang['List pictures of this album'] = 'Liste bildene i denne album';
