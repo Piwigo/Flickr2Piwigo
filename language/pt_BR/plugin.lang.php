38bfdef8ec537b7c88ab40a26965ff7b5ade80c1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-04-17 06:21:58 +0200

    Update pt_BR, thanks to flaviove

--- language/pt_BR/plugin.lang.php
+++ language/pt_BR/plugin.lang.php
@@ -70,3 +70,4 @@ $lang['circa'] = 'circa';
 $lang['month'] = 'a cerca de';
 $lang['year'] = 'ano';
 $lang['Unable to write to Flickr2Piwigo cache directory: %s'] = 'Não é possível gravar no diretório de cache do Flickr2Piwigo: %s';
+$lang['View counter'] = 'Visualizar contador';
