f14414b2a2a523078d23a7ad6ff5277ae2bf2fc3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-11-11 08:13:27 +0100

    Add ca_ES, thanks to Tirs

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['month'] = 'mes';
+$lang['year'] = 'any';
+$lang['%d albums'] = '%d àlbums';
+$lang['%d elements ready for importation'] = '%d elements llestos per a importar';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d imatges no es mostren perquè ja existeixen a la base de dades.';
+$lang['%d pictures imported'] = '%d imatges importades';
+$lang['(%d photos)'] = '(%d fotos)';
+$lang['API key'] = 'Clau d\'API';
+$lang['API not authenticated'] = 'API no autenticada';
+$lang['API secret'] = 'secret d\'API';
+$lang['Already imported: %s (Piwigo ID: %s)'] = 'Ja importat: %s (ID de Piwigo: %s)';
+$lang['An error occurred when trying to log in to Flickr: %s'] = 'S\'ha produït un error en intentar connectar-se a Flickr: %s';
+$lang['Begin transfer'] = 'Iniciar la transferència';
+$lang['Callback URL'] = 'URL de crida de retorn (callback)';
+$lang['Can\'t download file: %s'] = 'No es pot descarregar el fitxer: %s';
+$lang['Create a new album'] = 'Crea un nou àlbum';
+$lang['Fill these fields from Flickr datas'] = 'Omple aquests camps a partir de les dades de Flickr';
+$lang['Flickr logins'] = 'Testimoni de l\'aplicació de Flickr';
+$lang['Geolocalization'] = 'Geolocalització';
+$lang['Help'] = 'Ajuda';
+$lang['How do I get my Flickr API key?'] = 'Com puc obtenir la meva clau d\'API de Flickr?';
+$lang['Import'] = 'Importa';
+$lang['Import all my pictures'] = 'Importa totes les meves imatges';
+$lang['Import all photos in this album'] = 'Importa totes les fotos d\'aquest àlbum';
+$lang['Import options'] = 'Opcions d\'importació';
+$lang['Imported from Flickr'] = 'Importat des de Flickr';
+$lang['List my albums'] = 'Llista els meus àlbums';
+$lang['List pictures of this album'] = 'Llista les imatges d\'aquest àlbum';
+$lang['Logged in as'] = 'Connectat com';
+$lang['Logged out'] = 'Desconnectat';
+$lang['Moderate'] = 'Moderat';
+$lang['No download method available'] = 'No hi ha cap mètode de descàrrega disponible';
+$lang['One picture is not displayed because already existing in the database.'] = 'Una imatge no es mostra perquè ja existeix a la base de dades.';
+$lang['Open Flickr page in a new tab'] = 'Obre la pàgina de Flickr en una nova pestanya';
+$lang['Photo "%s" imported'] = 'S\'ha importat la foto "%s"';
+$lang['Pictures without album'] = 'Imatges sense àlbum';
+$lang['Please enter your Flickr API keys'] = 'Introduïu les vostres claus de l\'API de Flickr';
+$lang['Please enter your Flickr API keys on the configuration tab'] = 'Introduïu les vostres claus de l\'API de Flickr a la pestanya de configuració';
+$lang['Processing...'] = 'Processant...';
+$lang['Reproduce flickr albums'] = 'Reprodueix els àlbums de Flickr';
+$lang['Restricted'] = 'Restringit';
+$lang['Safety level (as keyword)'] = 'Nivell de seguretat (com a paraula clau)';
+$lang['Stop'] = 'Atura';
+$lang['Success'] = 'Èxit';
+$lang['Successfully logged in to your Flickr account'] = 'Connectat correctament al vostre compte de Flickr';
+$lang['Unable to create category: %s'] = 'No es pot crear la categoria: %s';
+$lang['Unable to get info from Flickr about photo: %s'] = 'No es pot obtenir informació des de Flickr sobre la foto: %s';
+$lang['Unable to write to Flickr2Piwigo cache directory: %s'] = 'No es pot escriure al directori cau de Flickr2Piwigo: %s';
+$lang['View counter'] = 'Visualitza comptador';
+$lang['approximate date'] = 'Data aproximada';
+$lang['c. %s'] = 'aprox. %s';
+$lang['circa'] = 'aproximadament';
