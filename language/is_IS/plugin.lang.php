b4e9c96e606be675ca89178298920a0a95810f78
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-06 05:45:08 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['%d albums'] = '%d albúm';
+$lang['%d elements ready for importation'] = '%d atriði tilbúin til innflutnings';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d myndir eru ekki birtar því þær eru þegar til í gagnagrunninum.';
+$lang['%d pictures imported'] = '%d myndir fluttar inn';
+$lang['(%d photos)'] = '(%d myndir)';
+$lang['API key'] = 'API lykill';
+$lang['API not authenticated'] = 'API ekki staðfest';
+$lang['API secret'] = 'API leyniorð';
+$lang['Already imported: %s (Piwigo ID: %s)'] = 'Þegar flutt inn: %s (Piwigo auðkenni: %s)';
+$lang['An error occurred when trying to log in to Flickr: %s'] = 'Villa kom upp við innskráningu á Flickr: %s';
+$lang['Begin transfer'] = 'Hefja flutning';
+$lang['Callback URL'] = 'Afturkallsvefslóð';
+$lang['Can\'t download file: %s'] = 'Ekki er hægt að sækja skrána: %s';
+$lang['Create a new album'] = 'Stofna nýtt albúm';
+$lang['Fill these fields from Flickr datas'] = 'Fyllið út þessa reiti úr Flickr gögnum';
+$lang['Flickr logins'] = 'Tákn fyrir Flickr appið';
+$lang['Geolocalization'] = 'Landfræðileg staðsetning';
+$lang['Help'] = 'Hjálp';
+$lang['How do I get my Flickr API key?'] = 'Hvernig fæ ég Flickr API lykilinn minn?';
+$lang['Import'] = 'Innflutningur';
