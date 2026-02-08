b01ac526830d4937c046c9a1e44a3166a4dd24a4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-08 08:14:23 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Already imported: %s (Piwigo ID: %s)'] = 'כבר יובא: %s (מזהה Piwigo: %s)';
+$lang['Can\'t download file: %s'] = 'לא ניתן להוריד את הקובץ: %s';
+$lang['How do I get my Flickr API key?'] = 'איך מקבלים את מפתח ה-Flickr API שלי?';
+$lang['Successfully logged in to your Flickr account'] = 'התחברתם בהצלחה לחשבון ה-Flickr שלכם';
+$lang['Unable to get info from Flickr about photo: %s'] = 'לא ניתן לקבל מידע מ-Flickr על התמונה: %s';
+$lang['c. %s'] = 'בערך %s';
+$lang['Import all my pictures'] = 'ייבוא כל התמונות שלי';
+$lang['Import all photos in this album'] = 'ייבוא כל התמונות באלבום זה';
+$lang['List pictures of this album'] = 'רשימת התמונות של אלבום זה';
+$lang['Logged in as'] = 'מחובר כ-';
+$lang['Logged out'] = 'התנתקות';
+$lang['Moderate'] = 'ניהול';
+$lang['One picture is not displayed because already existing in the database.'] = 'תמונה אחת לא מוצגת מכיוון שהיא כבר קיימת במסד הנתונים.';
+$lang['Please enter your Flickr API keys'] = 'נא להזין את מפתחות ה-API של Flickr';
+$lang['Please enter your Flickr API keys on the configuration tab'] = 'נא להזין את מפתחות ה-API של Flickr בלשונית ההגדרות';
+$lang['Reproduce flickr albums'] = 'שחזור אלבומי flickr';
+$lang['%d elements ready for importation'] = '%d פריטים מוכנים לייבוא';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d תמונות אינן מוצגות כי הן כבר קיימות במסד הנתונים.';
+$lang['Begin transfer'] = 'התחלת העברה';
+$lang['Callback URL'] = 'כתובת Callback';
+$lang['Fill these fields from Flickr datas'] = 'מלאו את השדות מנתוני Flickr';
+$lang['Flickr logins'] = 'אסימון אפליקציית Flickr';
+$lang['Geolocalization'] = 'גאולוקליזציה';
+$lang['Stop'] = 'עצור';
+$lang['Success'] = 'הצלחה';
+$lang['Unable to create category: %s'] = 'לא ניתן ליצור קטגוריה: %s';
+$lang['Unable to write to Flickr2Piwigo cache directory: %s'] = 'לא ניתן לכתוב לתיקיית המטמון של Flickr2Piwigo: %s';
+$lang['View counter'] = 'מונה צפיות';
+$lang['approximate date'] = 'תאריך משוער';
+$lang['circa'] = 'בערך';
+$lang['month'] = 'חודש';
+$lang['year'] = 'שנה';
+$lang['Import options'] = 'אפשרויות ייבוא';
+$lang['Imported from Flickr'] = 'יובא מ-Flickr';
+$lang['List my albums'] = 'רשימת האלבומים שלי';
+$lang['No download method available'] = 'אין שיטת הורדה זמינה';
+$lang['Open Flickr page in a new tab'] = 'פתח דף Flickr בכרטיסייה חדשה';
+$lang['Photo "%s" imported'] = 'התמונה "%s" יובאה';
+$lang['Pictures without album'] = 'תמונות ללא אלבום';
+$lang['Processing...'] = 'מעבד...';
+$lang['Restricted'] = 'מוגבל';
+$lang['Safety level (as keyword)'] = 'רמת בטיחות (כמילת מפתח)';
+$lang['%d albums'] = '%d אלבומים';
+$lang['%d pictures imported'] = '%d תמונות יובאו';
+$lang['(%d photos)'] = '(%d תמונות)';
+$lang['API key'] = 'מפתח API';
+$lang['API not authenticated'] = 'API לא מאומת';
+$lang['API secret'] = 'סוד API';
+$lang['An error occurred when trying to log in to Flickr: %s'] = 'אירעה שגיאה בעת ניסיון להתחבר ל-Flickr: %s';
+$lang['Create a new album'] = 'צור אלבום חדש';
+$lang['Help'] = 'עזרה';
+$lang['Import'] = 'ייבוא';
