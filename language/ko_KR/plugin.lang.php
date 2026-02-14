309d0d4160ee3200f36ee4ab0b20f8f20955a39e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-14 13:03:06 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Stop'] = '중지';
+$lang['Success'] = '성공';
+$lang['Successfully logged in to your Flickr account'] = 'Flickr 계정에 성공적으로 로그인했습니다';
+$lang['Unable to create category: %s'] = '카테고리 생성 실패: %s';
+$lang['Unable to get info from Flickr about photo: %s'] = 'Flickr에서 사진 정보 가져오기 실패: %s';
+$lang['Unable to write to Flickr2Piwigo cache directory: %s'] = 'Flickr2Piwigo 캐시 디렉터리에 쓰기 실패: %s';
+$lang['View counter'] = '조회수 카운터';
+$lang['approximate date'] = '대략적인 날짜';
+$lang['c. %s'] = 'c. %s';
+$lang['circa'] = 'circa';
+$lang['month'] = '월';
+$lang['year'] = '년';
+$lang['Import all my pictures'] = '내 사진 모두 가져오기';
+$lang['Import all photos in this album'] = '이 앨범의 사진 모두 가져오기';
+$lang['Import options'] = '가져오기 옵션';
+$lang['Imported from Flickr'] = 'Flickr에서 가져옴';
+$lang['List my albums'] = '내 앨범 목록';
+$lang['List pictures of this album'] = '이 앨범의 사진 목록';
+$lang['Logged in as'] = '로그인 상태';
+$lang['Logged out'] = '로그아웃됨';
+$lang['Moderate'] = '관리 중';
+$lang['No download method available'] = '사용 가능한 다운로드 방법이 없습니다';
+$lang['One picture is not displayed because already existing in the database.'] = '이미 데이터베이스에 존재하기 때문에 한 장의 사진이 표시되지 않습니다.';
+$lang['Open Flickr page in a new tab'] = '새 탭에서 Flickr 페이지 열기';
+$lang['Photo "%s" imported'] = '사진 "%s" 가져옴';
+$lang['Pictures without album'] = '앨범이 없는 사진들';
+$lang['Please enter your Flickr API keys'] = 'Flickr API 키를 입력해 주세요';
+$lang['Please enter your Flickr API keys on the configuration tab'] = '설정 탭에서 Flickr API 키를 입력해 주세요';
+$lang['Processing...'] = '처리 중...';
+$lang['Reproduce flickr albums'] = 'Flickr 앨범 재현';
+$lang['Restricted'] = '제한됨';
+$lang['Safety level (as keyword)'] = '안전 수준 (키워드로)';
+$lang['%d albums'] = '%d개의 앨범';
+$lang['%d elements ready for importation'] = '%d개의 요소가 가져오기 준비 완료';
+$lang['%d pictures are not displayed because already existing in the database.'] = '%d개의 사진은 데이터베이스에 이미 존재하므로 표시되지 않습니다.';
+$lang['%d pictures imported'] = '%d개의 사진 가져옴';
+$lang['(%d photos)'] = '(%d개의 사진)';
+$lang['API key'] = 'API 키';
+$lang['API not authenticated'] = 'API 인증 실패';
+$lang['API secret'] = 'API 비밀번호';
+$lang['Already imported: %s (Piwigo ID: %s)'] = '이미 가져옴: %s (Piwigo ID: %s)';
+$lang['An error occurred when trying to log in to Flickr: %s'] = 'Flickr 로그인 시도 중 오류 발생: %s';
+$lang['Begin transfer'] = '전송 시작';
+$lang['Callback URL'] = '콜백 URL';
+$lang['Can\'t download file: %s'] = '파일 다운로드 실패: %s';
+$lang['Create a new album'] = '새 앨범 생성';
+$lang['Fill these fields from Flickr datas'] = 'Flickr 데이터로 이 필드 채우기';
+$lang['Flickr logins'] = 'Flickr 앱 토큰';
+$lang['Geolocalization'] = '지리적 위치 정보';
+$lang['Help'] = '도움말';
+$lang['How do I get my Flickr API key?'] = 'Flickr API 키는 어떻게 얻나요?';
+$lang['Import'] = '가져오기';
